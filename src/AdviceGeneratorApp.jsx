fcaac846870fd26b15041959fc30790875d7ae8f
newt <axelskam.123@outlook.es>
2022-12-15 14:11:19 -0500

    initi advice project

---
+++ src/AdviceGeneratorApp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AdviceGeneratorApp = () => {
+  return <div>AdviceGeneratorApp</div>;
+};
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AdviceGeneratorApp } from "./AdviceGeneratorApp";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AdviceGeneratorApp />
+  </React.StrictMode>
+);
